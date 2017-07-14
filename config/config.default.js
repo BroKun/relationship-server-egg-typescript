15fed4e78791c12fa3fd7f26d4cb62442e524310
王玉坤 <wang_yk@hzzh.com>
2017-07-14 23:37:15 +0800

    挖个坑

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1500019401789_9482';
+
+  // add your config here
+
+  return config;
+};
