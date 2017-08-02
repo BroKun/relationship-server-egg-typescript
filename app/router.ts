f10f07ddfcf138e81056378ee528ccd1ee7f7cb2
BroKun <wangyukun1234@hotmail.com>
2017-08-02 19:35:18 +0800

    add Application declare

--- app/router.ts
+++ app/router.ts
@@ -1,6 +1,11 @@
 'use strict';
 import { Application } from 'egg';
 
+declare module 'egg' {
+  export interface Application {
+    validator: any;
+  }
+}
 module.exports = (app: Application) => {
   app.get('/', 'home.index');
   app.resources('test', '/api/v1/test', 'test');
