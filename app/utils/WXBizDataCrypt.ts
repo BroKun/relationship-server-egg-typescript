00c9513fec69109cba761cd096cb69687f3f64da
BroKun <wangyukun1234@hotmail.com>
2017-08-20 23:56:23 +0800

    转换为ts

--- app/utils/WXBizDataCrypt.ts
+++ app/utils/WXBizDataCrypt.ts
@@ -1,19 +1,17 @@
-'use strict';
-
-const crypto = require('crypto');
+import * as crypto from 'crypto';
 
 function WXBizDataCrypt(appId, sessionKey) {
   this.appId = appId;
   this.sessionKey = sessionKey;
 }
 
-WXBizDataCrypt.prototype.decryptData = function(encryptedData, iv) {
+WXBizDataCrypt.prototype.decryptData = function decryptData (encryptedData, iv) {
   // base64 decode
   const sessionKey = new Buffer(this.sessionKey, 'base64');
   encryptedData = new Buffer(encryptedData, 'base64');
   iv = new Buffer(iv, 'base64');
 
-  let decoded = '';
+  let decoded: any = '';
   try {
     // 解密
     const decipher = crypto.createDecipheriv('aes-128-cbc', sessionKey, iv);
