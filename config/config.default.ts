443d4ebc6a68a92158024dd90a7afc03cf8f15e3
BroKun <wangyukun1234@hotmail.com>
2017-08-20 23:56:45 +0800

    jwt从默认配置移出

--- config/config.default.ts
+++ config/config.default.ts
@@ -16,22 +16,17 @@ declare module 'egg' {
       secret: string;
       key: string;
       ignore?: any;
-    }
+    };
   }
 }
 
 module.exports = (appInfo: EggAppConfig) => {
   const config: any = {};
   config.keys = appInfo.name + '123456';
-  config.middleware = [ 'errorHandler', 'jwt' ];
+  config.middleware = [ 'errorHandler'];
   config.wxapp = {
     AppID: 'wxb736f611f652ac16',
     AppSecret: '0693d9005ce2c0886c194c3aa9e07a4a',
-  };
-  config.jwt = {
-    secret: 'escape master',
-    key: 'user',
-    ignore: '/login',
   };
   return config;
 };
--- config/config.prod.ts
+++ config/config.prod.ts
@@ -3,6 +3,12 @@ import 'source-map-support/register';
 
 module.exports = (appInfo: EggAppConfig) => {
   const config: any = {};
+  config.middleware = ['jwt' ];
+  config.jwt = {
+    secret: 'escape master',
+    key: 'user',
+    ignore: '/login',
+  };
   return config;
 };
 
