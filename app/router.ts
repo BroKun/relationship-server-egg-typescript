09d03d47dbd753d587e6eb17f7f5564f0bbddf7a
BroKun <wangyukun1234@hotmail.com>
2017-08-21 23:54:34 +0800

    修改token路由

--- app/router.ts
+++ app/router.ts
@@ -1,6 +1,6 @@
 import { Application } from 'egg';
 
 module.exports = (app: Application) => {
-  app.get('user/token', app.controller.user.token);
+  app.get('/user/token', app.controller.user.token);
   app.resources('test', '/api/v1/test', 'test');
 };
