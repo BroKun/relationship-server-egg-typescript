f40dad94ebc4ae671272b6a5fd69fc42707681a8
BroKun <wangyukun1234@hotmail.com>
2017-09-20 19:14:19 +0800

    增加单元测试配置

---
+++ config/config.unittest.ts
@@ -0,0 +1,26 @@
+import { EggAppConfig } from 'egg';
+import 'source-map-support/register';
+
+export interface UnitTest {
+  middleware: Array<string>;
+  jwt: {
+    secret: string;
+    key: string;
+    ignore?: any;
+  };
+}
+
+export default (appInfo: EggAppConfig):UnitTest => {
+  return {
+    middleware: ['errorHandler', 'jwt'],
+    jwt: {
+      secret: 'escape master',
+      key: 'user',
+      ignore: '/user/token',
+    },
+  };
+};
+
+export const proxyworker = {
+  port: 8080,
+};
