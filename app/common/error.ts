40dfdd3b1cdccbdc0e52160ee084046d09b00ae6
BroKun <wangyukun1234@hotmail.com>
2017-09-20 19:12:04 +0800

    注释

--- app/common/error.ts
+++ app/common/error.ts
@@ -1,5 +1,11 @@
 export default class ErrorRes<T> extends Error {
+  /**
+   * 状态码
+   */
   status: number;
+  /**
+   * 错误信息
+   */
   errors: T;
   constructor(status: number, error: T) {
     super();
