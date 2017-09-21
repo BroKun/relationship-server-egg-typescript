8f86479617483dc90b31fad9a620931583f61f7b
BroKun <wangyukun1234@hotmail.com>
2017-09-21 20:58:03 +0800

    remove debug proxy

--- config/plugin.ts
+++ config/plugin.ts
@@ -2,10 +2,6 @@ export const validate = {
   enable: true,
   package: 'egg-validate',
 };
-export const proxyworker = {
-  enable: true,
-  package: 'egg-development-proxyworker',
-};
 export const mongoose = {
   enable: true,
   package: 'egg-mongoose',
