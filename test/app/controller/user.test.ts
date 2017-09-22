079f86e17dc1904646617b030e5c4865191d0db8
BroKun <wangyukun1234@hotmail.com>
2017-09-22 18:36:48 +0800

    add

---
+++ test/app/controller/user.test.ts
@@ -0,0 +1,24 @@
+import * as mm from 'egg-mock';
+
+describe('token管理', () => {
+  const app = mm.app();
+  before(async () => {
+    await app.ready();
+  });
+
+  after(() => app.close());
+
+  afterEach(mm.restore);
+
+  it('真实请求,创建User', () => {
+    app.mockCsrf();
+    return app.httpRequest()
+      .post('/api/user')
+      .send({
+        realName: '张三',
+        enrollmentYear: 2001,
+        openId: '11111',
+      })
+      .expect(201);
+  });
+});
