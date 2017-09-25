0591472fa0c7ea3bad0e1de3eebd6a8e3f301508
BroKun <wangyukun1234@hotmail.com>
2017-09-25 15:25:51 +0800

    修改发布配置

--- config/config.default.ts
+++ config/config.default.ts
@@ -1,9 +1,12 @@
 import { EggAppConfig } from 'egg';
-import * as fs from 'fs';
-import * as yaml from 'js-yaml';
 import 'source-map-support/register';
 
-interface Privacy {
+export interface Default {
+  /**
+   * token加解密
+   */
+  tokenSecret: string;
+  keys: string;
   mongoose: {
     url: string;
     option: object;
@@ -12,19 +15,29 @@ interface Privacy {
     AppID: string;
     AppSecret: string;
   };
+  security: {
+    csrf: {
+      enable: boolean;
+    },
+  };
 }
-export type Default = Privacy & {
-  /**
-   * token加解密
-   */
-  tokenSecret: string;
-  keys: string;
-};
 
 export default (appInfo: EggAppConfig): Default => {
-  const privacyInfo: Privacy = yaml.safeLoad(fs.readFileSync(`config/config.yml`, 'utf8'));
-  const config: Default = {
-    keys: appInfo.name + '123456',
-    tokenSecret: '', ...privacyInfo};
-  return config;
+  return {
+    keys: appInfo.name + 'escape-master',
+    tokenSecret: '',
+    mongoose: {
+      url: 'mongodb://mongo:test@101.200.36.181:27017/test',
+      option: {},
+    },
+    wxapp: {
+      AppID: 'wxb736f611f652ac16',
+      AppSecret: '0693d9005ce2c0886c194c3aa9e07a4a',
+    },
+    security: {
+      csrf: {
+        enable: false,
+      },
+    },
+  };
 };
