ccf84684822c0fc4181ba3a43aa9d7f4f67e7c6f
BroKun <wangyukun1234@hotmail.com>
2017-10-10 22:50:05 +0800

    fix lint

--- test/app/util/httpUtil.test.ts
+++ test/app/util/httpUtil.test.ts
@@ -2,34 +2,34 @@
  * Created by sun48 on 2017/10/3.
  */
 import * as assert from 'assert';
-import {http, ReqOps} from '../../../app/utils/httpUtil';
+import { http, ReqOps } from '../../../app/utils/httpUtil';
 
 describe('httpUtil工具测试', () => {
-  let headers = {
+  const headers = {
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Content-Length': 0
+    'Content-Length': 0,
   };
-  it('test http请求', async() => {
+  it('test http请求', async () => {
     const ops = new ReqOps(
       `jsonplaceholder.typicode.com`,
       `/posts/1`,
       `GET`,
       80,
-      headers
+      headers,
     );
-    let res = JSON.parse(await http(ops));
-    assert(res.id == 1);
+    const res = JSON.parse(await http(ops));
+    assert(res.id === 1);
   });
 
-  it('test错误的http请求', async() => {
+  it('test错误的http请求', async () => {
     const ops = new ReqOps(
       `jsonplaceholder.typicode.com`,
       `/post`,
       `GET`,
       80,
-      headers
+      headers,
     );
-    let res = JSON.parse(await http(ops));
-    assert(JSON.stringify(res) == '{}');
+    const res = JSON.parse(await http(ops));
+    assert(JSON.stringify(res) === '{}');
   });
 });
