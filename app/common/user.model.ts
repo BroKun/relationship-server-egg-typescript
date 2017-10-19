0243dd08caf75b239d719b17332b99eb4d912b3b
BroKun <wangyukun1234@hotmail.com>
2017-10-19 21:02:50 +0800

    update user Model

---
+++ app/common/user.model.ts
@@ -0,0 +1,72 @@
+import { Document, Schema } from 'mongoose';
+
+export interface IUserBase extends Document {
+  /**
+   * 用户Id
+   */
+  _id: string;
+  /**
+   * openId,来自微信
+   */
+  openId: string;
+  /**
+   * unionId,来自微信
+   */
+  unionId?: string;
+  /**
+   * 真实姓名
+   */
+  realName?: string;
+  /**
+   * 昵称
+   */
+  nickName: string;
+  /**
+   * 头像
+   */
+  avatar: string;
+}
+
+export const userBaseSchema = {
+  _id: { type: Schema.Types.ObjectId, ref: 'User', required: true, index: true },
+  openId: { type: Schema.Types.String, required: true, index: true },
+  unionId: { type: Schema.Types.String, required: false },
+  realName: { type: Schema.Types.String, required: false },
+  nickName: { type: Schema.Types.String, required: true, index: true },
+  avatar: { type: Schema.Types.String, required: false },
+};
+
+export interface IUser extends IUserBase {
+  /**
+   * 入学年份
+   */
+  enrollmentYear?: number;
+  /**
+   * 专业
+   */
+  major?: string;
+  /**
+   * 签名
+   */
+  bio?: string;
+  /**
+   * 正式成员
+   */
+  member: boolean;
+  /**
+   * 师傅
+   */
+  masters?: IUserBase;
+  /**
+   * 徒弟
+   */
+  apprentices?: [IUserBase];
+  /**
+   * 创建时间
+   */
+  createAt: Date;
+  /**
+   * 修改时间
+   */
+  updateAt: Date;
+}
