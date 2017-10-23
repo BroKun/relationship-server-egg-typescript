4b4ca97b41062d82da05727102c68a205bb36b20
BroKun <wangyukun1234@hotmail.com>
2017-10-23 21:40:41 +0800

    update

--- app/controller/tokens.ts
+++ app/controller/tokens.ts
@@ -1,6 +1,7 @@
 import { Controller, DefaultConfig } from 'egg';
 import * as jwt from 'jsonwebtoken';
 import * as moment from 'moment';
+import { Token } from '../common/tokens.model';
 
 interface SessionInfo {
   openid: string;
@@ -16,6 +17,10 @@ export default class Tokens extends Controller {
     const wxRes = await ctx.service.wechat.jscode2session(ctx.request.body['code']);
     if ((wxRes as WX.Error).errcode) { ctx.throw(422, {errors: wxRes}); }
     const exp: number = moment().add(30, 'days').unix();
+    const user = await ctx.model.User.findOne({ _id: ctx.params.id });
+    if (!user) {
+      ctx.throw(403, 'User Not Found');
+    }
     const token: string = jwt.sign({ ...wxRes, exp }, (config as DefaultConfig).jwt.secret);
     const res: SessionInfo = {
       token,
