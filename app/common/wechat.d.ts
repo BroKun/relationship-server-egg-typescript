acd51ecd2652542ab2d5e6c2792d6b036266b302
BroKun <wangyukun1234@hotmail.com>
2017-10-23 21:39:55 +0800

    增加微信api返回值定义

--- app/common/wechat.d.ts
+++ app/common/wechat.d.ts
@@ -5,33 +5,98 @@ declare namespace WX {
      * 微信平台openId
      */
     openid: string;
+    /**
+     * 用户session_key,有效期由微信服务器控制
+     */
     session_key: string;
-    unionid: string;
+    /**
+     * 联合Id
+     * 适用于同一认证主体
+     */
+    unionid?: string;
   }
+
   /**
    * 小程序API定义的错误信息格式
    */
   interface Error {
+    /**
+     * 异常编码
+     */
     errcode: number;
+    /**
+     * 异常信息
+     */
     errmsg: string;
   }
-  interface RawData {
+
+  /**
+   * 用户基本信息
+   */
+  interface UserInfo {
+    /**
+     * 昵称
+     */
     nickName: string;
+    /**
+     * 性别
+     * 0：未知、1：男、2：女
+     */
     gender: number;
-    language: string;
+    /**
+     * 城市
+     */
     city: string;
+    /**
+     * 省份
+     */
     province: string;
+    /**
+     * 国家
+     */
     country: string;
+    /**
+     * 头像地址
+     */
     avatarUrl: string;
   }
+
+  /**
+   * 用户加密信息
+   */
   interface EncryptData {
+    /**
+     * 微信平台openId
+     */
     openId: string;
+    /**
+     * 昵称
+     */
     nickName: string;
+    /**
+     * 性别
+     * 0：未知、1：男、2：女
+     */
     gender: number;
+    /**
+     * 城市
+     */
     city: string;
+    /**
+     * 省份
+     */
     province: string;
+    /**
+     * 国家
+     */
     country: string;
+    /**
+     * 头像地址
+     */
     avatarUrl: string;
+    /**
+     * 联合id
+     */
     unionId: string;
   }
 }
