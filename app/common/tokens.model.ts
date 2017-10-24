02044453d40b0d99dddf0a7908c2aebcfbbcaa8a
BroKun <wangyukun1234@hotmail.com>
2017-10-24 22:55:34 +0800

    :art: token 类型与生成机制

--- app/common/tokens.model.ts
+++ app/common/tokens.model.ts
@@ -1,11 +1,9 @@
-import { IUser } from './users.model';
-
-export class Token {
-  userInfo: IUser;
+import { SignOptions } from 'jsonwebtoken';
+export interface ITokenBase {
   /**
-   * 用户Id
+   * 生成时间
    */
-  _id: string;
+  iat: number;
   /**
    * 有效期至
    */
@@ -15,41 +13,56 @@ export class Token {
    */
   nbf?: number;
   /**
-   * 生成时间
-   */
-  iat?: number;
-  /**
    * 接受者
    */
-  aud?: string;
+  aud?: string | number;
   /**
    * 主题
    */
-  sub?: string;
+  sub?: string | number;
   /**
    * 来源
    */
-  iss?: string;
-  constructor(user: IUser, exp?: number, nbf?: number, iat?: number, aud?: string, sub?: string, iss?: string) {
-    this.userInfo = user;
-    this._id = user._id;
-    if (exp) {
-      this.exp = exp;
-    }
-    if (nbf) {
-      this.nbf = nbf;
-    }
-    if (iat) {
-      this.iat = iat;
-    }
-    if (aud) {
-      this.aud = aud;
-    }
-    if (sub) {
-      this.sub = sub;
-    }
-    if (iss) {
-      this.iss = iss;
-    }
+  iss?: string | number;
+}
+
+export type Token<T> = ITokenBase & T;
+export class SignOptionsImpl implements SignOptions {
+  /**
+   * Signature algorithm. Could be one of these values :
+   * - HS256:    HMAC using SHA-256 hash algorithm (default)
+   * - HS384:    HMAC using SHA-384 hash algorithm
+   * - HS512:    HMAC using SHA-512 hash algorithm
+   * - RS256:    RSASSA using SHA-256 hash algorithm
+   * - RS384:    RSASSA using SHA-384 hash algorithm
+   * - RS512:    RSASSA using SHA-512 hash algorithm
+   * - ES256:    ECDSA using P-256 curve and SHA-256 hash algorithm
+   * - ES384:    ECDSA using P-384 curve and SHA-384 hash algorithm
+   * - ES512:    ECDSA using P-521 curve and SHA-512 hash algorithm
+   * - none:     No digital signature or MAC value included
+   */
+  algorithm?: string;
+  keyid?: string;
+  /**
+   * expressed in seconds or a string describing a time span
+   * Eg: 60, "2 days", "10h", "7d"
+   */
+  expiresIn?: string | number;
+  /**
+   * expressed in seconds or a string describing a time span
+   * Eg: 60, "2 days", "10h", "7d"
+   */
+  notBefore?: string | number;
+  audience?: string | string[];
+  subject?: string;
+  issuer?: string;
+  jwtid?: string;
+  noTimestamp?: boolean;
+  header?: object;
+  encoding?: string;
+  constructor() {
+    this.expiresIn = 60 * 60 * 24 * 7;
+    this.issuer = 'system';
+    this.subject = 'basic';
   }
 }
