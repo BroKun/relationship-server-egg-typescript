ad2f2ce3c79a61992bce6d9651a322c4a8658010
BroKun <wangyukun1234@hotmail.com>
2017-10-25 14:24:59 +0800

    增加正式会员验证方法

--- app/utils/authorized.ts
+++ app/utils/authorized.ts
@@ -1,10 +1,26 @@
 import { Controller, DefaultConfig } from 'egg';
+
 export default function authorized(target: any, propertyKey: string, descriptor: PropertyDescriptor): void {
   const method: () => {} = descriptor.value;
   function authCheck(): void {
     const key: string = (this.config as DefaultConfig).jwt.key;
     if ((this as Controller).ctx.state && (this as Controller).ctx.state[key]) {
       return method.call(this);
+    }
+    (this as Controller).ctx.throw(401);
+  }
+  descriptor.value = authCheck;
+}
+export function regular(target: any, propertyKey: string, descriptor: PropertyDescriptor): void {
+  const method: () => {} = descriptor.value;
+  function authCheck(): void {
+    const key: string = (this.config as DefaultConfig).jwt.key;
+    if ((this as Controller).ctx.state && (this as Controller).ctx.state[key]) {
+      const tokenInfo: Relationship.User = (this as Controller).ctx.state[key];
+      if (tokenInfo.type >= 1) {
+        return method.call(this);
+      }
+      (this as Controller).ctx.throw(403, 'Permission Denied');
     }
     (this as Controller).ctx.throw(401);
   }
