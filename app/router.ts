d327a32c634b6dee3fea3117eddbf5186aa61f0c
BroKun <wangyukun1234@hotmail.com>
2017-10-26 23:24:12 +0800

    :art: sort

--- app/router.ts
+++ app/router.ts
@@ -1,11 +1,42 @@
 import { Application } from 'egg';
 
 module.exports = (app: Application) => {
+
   /**
-   * tokens
-   * POST /tokens
+   * teaching
+   * POST /users/teaching/:userid
+   * DELETE /users/teaching/:userid
+   * GET /user/:master/teaching/:apprentices
    */
-  app.post('token_create', '/api/v1/tokens', 'tokens.create');
+  app.post('teaching_create', '/api/v1/api/v1/user/teaching/:id', 'teaching.create');
+  app.delete('teaching_delete', '/api/v1/api/v1/user/teaching/:id', 'teaching.destroy');
+  app.get('teaching_check', '/api/v1/users/:master/teaching/:apprentices', 'teaching.check');
+
+  /**
+   * starring
+   * POST /users/starred/:userid
+   * DELETE /users/starred/:userid
+   * GET /user/:userid/starred/:otheruser
+   * GET /user/:userid/starred?{page,per_page,order,sort}
+   * GET /user/:userid/stargazers?{page,per_page,order,sort}
+   */
+  app.post('starred_create', '/api/v1/user/starred/:id', 'starring.create');
+  app.delete('starred_delete', '/api/v1/user/starred/:id', 'starring.destroy');
+  app.get('starred_check', '/api/v1/users/:stargazer/starred/:starred', 'starring.check');
+  app.get('starred_index', '/api/v1/users/:id/starred', 'starring.show');
+  app.get('stargazers', '/api/v1/users/:id/stargazers', 'starring.stargazers');
+
+  /**
+   * apprentices
+   * GET /user/:userid/apprentices
+   */
+  app.get('apprentices_index', '/api/v1/users/:id/apprentices', 'apprentices.index');
+  /**
+   * masters
+   * GET /user/:userid/masters
+   */
+  app.get('masters_index', '/api/v1/users/:id/masters', 'masters.index');
+
   /**
    * users
    * POST /users
@@ -18,37 +49,8 @@ module.exports = (app: Application) => {
   app.put('user_update', '/api/v1/users/:id', 'users.update');
   app.get('user_index', '/api/v1/users', 'users.index');
   /**
-   * teaching
-   * POST /users/teaching/:userid
-   * DELETE /users/teaching/:userid
-   * GET /user/:master/teaching/:apprentices
+   * tokens
+   * POST /tokens
    */
-  app.post('teaching_create', '/api/v1/users/teaching/:id', 'teaching.create');
-  app.delete('teaching_delete', '/api/v1/users/teaching/:id', 'teaching.destroy');
-  app.get('teaching_check', '/users/:master/teaching/:apprentices', 'teaching.check');
-
-  /**
-   * starring
-   * POST /users/starred/:userid
-   * DELETE /users/starred/:userid
-   * GET /user/:userid/starred/:otheruser
-   * GET /user/:userid/starred?{page,per_page,order,sort}
-   * GET /user/:userid/stargazers?{page,per_page,order,sort}
-   */
-  app.post('starred_create', '/user/starred/:id', 'starring.create');
-  app.delete('starred_delete', '/user/starred/:id', 'starring.destroy');
-  app.get('starred_check', '/users/:stargazer/starred/:starred', 'starring.check');
-  app.get('starred_index', '/users/:id/starred?{page,per_page,order,sort}', 'starring.show');
-  app.get('stargazers', '/users/:id/stargazers?{page,per_page,order,sort}', 'starring.stargazers');
-
-  /**
-   * apprentices
-   * GET /user/:userid/apprentices
-   */
-  app.get('apprentices_index', '/users/:id/apprentices', 'apprentices.index');
-  /**
-   * masters
-   * GET /user/:userid/masters
-   */
-  app.get('masters_index', '/users/:id/masters', 'masters.index');
+  app.post('token_create', '/api/v1/tokens', 'tokens.create');
 };
