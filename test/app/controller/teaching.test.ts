6e5f3a354c8cc81083c02635e2195869e5a52126
youngnutcake <296763240@qq.com>
2017-10-27 23:11:24 +0800

    add teaching test

--- test/app/controller/teaching.test.ts
+++ test/app/controller/teaching.test.ts
@@ -4,11 +4,8 @@ import tokenGen from '../../utils/token';
 describe('师徒关系', () => {
   const app = mm.app();
 
-  let token: string = '';
-
   before(async () => {
     await app.ready();
-    token = tokenGen(app,'59e766cbd07ee90cb1b9817c');
   });
   after(() => app.close());
 
@@ -17,7 +14,7 @@ describe('师徒关系', () => {
   it('建立师徒关系',() => {
     return app.httpRequest()
       .post('/api/v1/users/teaching/59ea0940271de30cb5b79031')
-      .set('Authorization', `Bearer ${token}`)
+      .set('Authorization', `Bearer ${tokenGen(app)}`)
       .send({}) //_id: '59e766cbd07ee90cb1b9817c'
       .expect(204);
   });
@@ -25,16 +22,16 @@ describe('师徒关系', () => {
   it('查询师徒关系',async () => {
     return app.httpRequest()
       .get('/user/59ea0940271de30cb5b79031/teaching/59e766cbd07ee90cb1b9817c')
-      .set('Authorization', `Bearer ${token}`)
+      .set('Authorization', `Bearer ${tokenGen(app)}`)
       .send({})
       .expect(200);
   });
 
-  // it('删除师徒关系',async () => {
-  //   return app.httpRequest()
-  //     .delete('/api/v1/users/teaching/59ea0940271de30cb5b79031')
-  //     .set('Authorization', `Bearer ${token}`)
-  //     .send({}) //_id: '59e766cbd07ee90cb1b9817c'
-  //     .expect(204);
-  // });
+  it('删除师徒关系',async () => {
+    return app.httpRequest()
+      .delete('/api/v1/users/teaching/59ea0940271de30cb5b79031')
+      .set('Authorization', `Bearer ${tokenGen(app)}`)
+      .send({})
+      .expect(204);
+  });
 });
