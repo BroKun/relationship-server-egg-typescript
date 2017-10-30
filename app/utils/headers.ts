1c067bb549cb5ab47b69cbff39a39e85b5014c13
BroKun <wangyukun1234@hotmail.com>
2017-10-30 20:49:36 +0800

    :hammer: cacheHandler  refactoring cacheHandler不再已中间件的形式出现，改到controller装饰器里

--- app/utils/headers.ts
+++ app/utils/headers.ts
@@ -2,11 +2,29 @@ import { Controller } from 'egg';
 
 export default function cacheControl(controlStr?: string) {
   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor): void {
-    const method: () => {} = descriptor.value;
-    function cacheSet() {
-      (this as Controller).ctx.set('Cache-Control', controlStr ? controlStr : 'no-cache');
+    const method: () => Promise<void> = descriptor.value;
+    function setNoCache() {
+      (this as Controller).ctx.set('cache-control', 'no-cache');
       return method.call(this);
     }
-    descriptor.value = cacheSet;
+    async function setCache() {
+      const ctx = (this as Controller).ctx;
+      ctx.set('cache-control', controlStr);
+      await method.call(this);
+      if (ctx.status >= 200 && ctx.status < 300
+        && ctx.res.getHeader('cache-control') !== 'no-cache') {
+        let reqEtag = ctx.req.headers['if-none-match'];
+        if (typeof reqEtag === 'string') {
+          if (reqEtag.startsWith('W/')) {
+            reqEtag = reqEtag.substr(2, reqEtag.length - 2);
+          }
+        }
+        if (reqEtag && reqEtag.length > 0 && ctx.res.getHeader('etag') && ctx.res.getHeader('etag') === reqEtag) {
+          ctx.throw(304);
+        }
+      }
+      return;
+    }
+    descriptor.value = controlStr ? setCache : setNoCache;
   };
 }
