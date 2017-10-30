0781727e4e78612f62bcee23a15a8c20ee0705ab
BroKun <wangyukun1234@hotmail.com>
2017-10-30 22:42:34 +0800

    init docs

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "dreamtecher",
+  "version": "1.0.0",
+  "description": "a Wechat mini-programs for dreamtecher",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-10-30T14:41:41.292Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
