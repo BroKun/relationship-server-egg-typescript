3c1067d7782dae33f7cb8c5d171450f95e6e5f54
brokun <brokun0128@gmail.com>
2018-06-05 04:52:09 +0800

    :fire: 微信接口变动，移除相关过时的代码

--- app/common/wechat.model.ts
+++
@@ -1,6 +0,0 @@
-export function isError(res: WX.Session | WX.Error): res is WX.Error {
-  if (res && (res as WX.Error).errcode) {
-    return true;
-  }
-  return false;
-}
--- app/utils/constant.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 静态信息，微信Api的访问地址
- */
-export default {
-  /**
-   * 根据数据编码获得微信session信息
-   */
-  jscode2session: 'api.weixin.qq.com/sns/jscode2session',
-};
